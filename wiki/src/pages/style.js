f5c5a29fc7ad24c122c50777bcb783a6fd2f6f39
Hudson Retonde <hudsonsretonde@gmail.com>
2022-12-03 16:37:19 -0300

    Style Container Inicial e Logo do GitHub

---
+++ wiki/src/pages/style.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    width: 100%;
+    heigth: 100vh;
+    padding-top: 20px;
+
+    display: flex;
+    alignItems: flex-start;
+    justify-content: center;
+
+`
