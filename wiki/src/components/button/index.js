d5c2118828104bdcb4136ac1a65a69d82a679307
Hudson Retonde <hudsonsretonde@gmail.com>
2022-12-05 14:59:35 -0300

    button component

---
+++ wiki/src/components/button/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { ButtonContainer } from './style';
+
+function Button(){
+    return(
+        <ButtonContainer>
+            Buscar
+        </ButtonContainer>
+    )
+}
+
+export default Button
---
+++ wiki/src/components/button/style.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const ButtonContainer = styled.button`
+
+    background-color: #FAFAFA;
+    border: 1px solid #FAFAFA;
+    border-radius: 20px;
+
+    height: 62px;
+    width: 80%;
+    margin: 20px;
+    font-size: 32px;
+    transition: 0.8s;
+
+    &: hover {
+        background-color: #ffa500;
+        color: #0000FF;
+        cursor: pointer;
+    }
+    
+`
