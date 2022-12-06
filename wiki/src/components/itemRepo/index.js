4955a7550f999a02ee1810dadf8aa612f349b13a
Hudson Retonde <hudsonsretonde@gmail.com>
2022-12-06 09:26:34 -0300

    Validando o input.

--- wiki/src/components/itemRepo/index.js
+++ wiki/src/components/itemRepo/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { ItemContainer } from "./style";
 
-function ItemRepo() {
+function ItemRepo({repo}) {
     return(
         <ItemContainer>
-            <h3> Hudson </h3>
-            <p>dio/Hudson</p>
-            <a href="#">Ver repositório</a> <br/>
-            <a href="#" className="remove">Remove</a>
+            <h3> {repo.name} </h3>
+            <p>{repo.full_name}</p>
+            <a href={repo.html_url} rel="noreferrer" target="_blank">Ver repositório</a> <br/>
+            <a href="#" rel="noreferrer" className="remove">Remove</a>
             <hr/>
         </ItemContainer>
     )
